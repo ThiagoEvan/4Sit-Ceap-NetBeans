793e303dcb1b88fb646f504d87550f18337a4f08
Thiago Evangelista Dias <45666190+ThiagoEvan@users.noreply.github.com>
2022-09-05 17:48:06 -0300

    Add files via upload

---
+++ Senha/src/Model/Senha_DAO.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Senha_DAO {
+
+    public static void prefenrencial(){
+        
+    }
+    
+    public static void naoPreferencial(){
+        
+    } 
+}
---
+++ Senha/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import View.tela_GUI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new tela_GUI().setVisible(true);
+    }
+    
+}
